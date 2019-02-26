85906e4cd58737d6b1e047212a813bef0e6429a3
apluser <apluser@slmgdevvm.jhuapl.edu>
2019-02-26 12:28:09 -0500

    initial commit of python function

---
+++ uniprot_xml_parser.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as etree
+
+
+def get_namespaces(path):
+    """
+    Grabs the various namesspaces in the xml document 
+    """
+    nsmap = {}
+    for event, elem in etree.iterparse(
+            path,
+            events=('start', 'end', 'start-ns', 'end-ns')):
+        if event == 'start-ns':
+            ns, url = elem
+            nsmap[ns] = url
+
+    print(nsmap)
+
+def parse_xml(path):
+    nsmap = {}
+    for event, elem in etree.iterparse(
+            path,
+            events=('start', 'end', 'start-ns', 'end-ns')):
+        print(elem)
+
+
+def parse_xml2(path):
+    parser = etree.XMLPullParser(events=('start','end'))
+    parser.feed(path)
+    print(list(parser.read_events()))
+
+def main():
+    #path = '/home/apluser/Data/xml_data/employees.xml'
+    #path = '/home/apluser/uniprot_test_small_2_entry.xml'
+    path = '/home/apluser/uniprot_sprot.xml'
+    #parse_xml2(path)
+    get_namespaces(path)
+    #parse_xml(path)
+
+if (__name__ == "__main__"):
+    main()
